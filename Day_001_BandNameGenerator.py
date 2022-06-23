d00819748cf9a8d390cf34a9a19872eb95e15768
lucy9696 <anetluciya.mec@gmail.com>
2022-06-23 16:47:09 +0200

    My second commit

--- Day_001_BandNameGenerator.py
+++ Day_001_BandNameGenerator.py
@@ -1,1 +1,7 @@
 # Learning Printing, Commenting, Debugging, String Manipulation and Variables as part of 100daysofcode
+# Output would look like this: https://replit.com/@appbrewery/band-name-generator-end.
+# Run the output console to find the bandnamegenerator.
+
+# Backstory: Suppose that your friend has a band and has to come up with a good name, so you can come to
+# to the rescue and can give them the band name generator program with city name and favourite pet.
+
